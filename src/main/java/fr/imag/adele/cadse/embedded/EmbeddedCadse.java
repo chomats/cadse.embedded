8a6f785a6e3e26c9abf91a234e702f8561dcf8c0
Stephane Chomat <Stephane.Chomat@imag.fr>
2010-03-23 17:07:19 +0100

    implements embedded cadse

--- src/main/java/fr/imag/adele/cadse/embedded/EmbeddedCadse.java
+++ src/main/java/fr/imag/adele/cadse/embedded/EmbeddedCadse.java
@@ -1,7 +1,14 @@
 package fr.imag.adele.cadse.embedded;
 
+import java.util.HashMap;
+import java.util.HashSet;
+
+import fr.imag.adele.cadse.core.CadseRuntime;
+import fr.imag.adele.cadse.core.LogicalWorkspace;
+import fr.imag.adele.cadse.core.WSModelState;
 import fr.imag.adele.cadse.core.impl.internal.CadseDomainImpl;
 import fr.imag.adele.cadse.workspace.si.defaultclassreferencer.DefaultClassReferencer;
+import fr.imag.adele.fede.workspace.as.initmodel.ErrorWhenLoadedModel;
 import fr.imag.adele.fede.workspace.si.initmodel.InitModel;
 import fr.imag.adele.fede.workspace.si.persistence.Persistence;
 import fr.imag.adele.teamwork.db.impl.ModelVersionDBImpl2;
@@ -14,9 +21,11 @@ public class EmbeddedCadse {
 	private EmbeddedBundleContext embeddedBundleContext;
 	ModelVersionDBImpl2 modelVersionDBImpl2;
 	EmbeddedFindModel embeddedFindModel;
-	
-	public EmbeddedCadse() {
-		persistence  = new Persistence();
+	private boolean readPersistence;
+
+	public EmbeddedCadse(boolean readPersistence) {
+		this.readPersistence = readPersistence;
+		persistence = new Persistence();
 		initModel = new InitModel();
 		initModel.classReferencer = new DefaultClassReferencer();
 		cadseDomainImpl = new CadseDomainImpl();
@@ -24,20 +33,81 @@ public class EmbeddedCadse {
 		mainPlatform = new EmbeddedPlatform();
 		embeddedBundleContext = new EmbeddedBundleContext();
 		mainPlatform._bundleContext = embeddedBundleContext;
-		modelVersionDBImpl2 = new ModelVersionDBImpl2(mainPlatform._bundleContext);
+		modelVersionDBImpl2 = new ModelVersionDBImpl2(
+				mainPlatform._bundleContext);
 		cadseDomainImpl._modelDB2Service = modelVersionDBImpl2;
 		initModel.workspaceCU = cadseDomainImpl;
 		initModel.platformService = mainPlatform;
 		embeddedFindModel = new EmbeddedFindModel();
 		initModel.findModel = embeddedFindModel;
 	}
-	
-	public void start() {
+
+	public void start() throws ErrorWhenLoadedModel {
 		cadseDomainImpl.start();
 		modelVersionDBImpl2.start();
-		initModel.loadCadses();
+
+		// load all cadse
+		CadseRuntime[] cadsePresents = initModel.loadCadses();
+
+		// find cadse to run
+		HashMap<String, CadseRuntime> crByName = new HashMap<String, CadseRuntime>();
+		for (CadseRuntime cr : cadsePresents) {
+			crByName.put(cr.getName(), cr);
+		}
+
+		String[] cadsesNameSaved = null;
+		if (readPersistence)
+			cadsesNameSaved = persistence.getCadsesName();
+		HashSet<CadseRuntime> toRun = new HashSet<CadseRuntime>();
+
+		if (cadsesNameSaved != null) {
+			for (String name : cadsesNameSaved) {
+				name = name.trim();
+				if (name.length() == 0) {
+					continue;
+				}
+				CadseRuntime cr = crByName.get(name);
+				if (cr != null) {
+					toRun.add(cr);
+				}
+			}
+		}
+		// look the properties CADSES_TO_EXECUTE
+		String addCadseName = System
+				.getProperty(LogicalWorkspace.CADSES_TO_EXECUTE);
+		if (addCadseName != null) {
+			String[] addCadseNameArray = addCadseName.split(",");
+
+			for (String name : addCadseNameArray) {
+				name = name.trim();
+				if (name.length() == 0) {
+					continue;
+				}
+				CadseRuntime cr = crByName.get(name);
+				if (cr != null) {
+					toRun.add(cr);
+				} else {
+					System.err.println("*** NOT FOUND CADSE " + name + " ***");
+				}
+			}
+		}
+		// load model
+		initModel.executeCadses(toRun.toArray(new CadseRuntime[toRun.size()]));
+
+		// cadsesNameSaved
+		LogicalWorkspace theCurrentModel = cadseDomainImpl
+				.getLogicalWorkspace();
+
+		theCurrentModel.setState(WSModelState.LOAD);
+		// load persistence
+		if (readPersistence && cadsesNameSaved != null) {
+			// if exits cadse to load
+			persistence.load();
+		}
+		theCurrentModel.setState(WSModelState.RUN);
+
 	}
-	
+
 	public void stop() {
 		cadseDomainImpl.stop();
 	}
